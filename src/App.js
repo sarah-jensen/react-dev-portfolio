1e44469e3e8fe51b3347bd1c4c8e38318c445c0b
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-06-28 00:19:56 -0700

    create Navbar.js component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { PacmanLoader} from 'react-spinners';
+import {
+  Navbar,
+  Header,
+  Footer,
+  Project
+} from './components';
 
-function App() {
+import { 
+  Container,
+  Typography, 
+  createTheme
+} from '@material-ui/core';
+
+
+const theme = createTheme(
+  {
+    palette: {
+      primary: {
+        main: '#00796b',
+      },
+      secondary: {
+        main: '#00838f',
+      },
+    },
+  }
+);
+
+const App =() => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+
     </div>
   );
 }
