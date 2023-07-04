0c9ec1c54162c81159809cee095dc6d82f7857e7
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-07-04 15:37:56 -0700

    add PageContainer.js and update navbar styling

---
+++ src/components/PageContainer.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+// import Contact from './pages/Contact';
+import { makeStyles } from '@material-ui/core/styles'
+
+export const PageContainer = () => {
+  const [currentPage, setCurrentPage] = useState('AboutMe');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'AboutMe') {
+      return <AboutMe />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    // return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+    </div>
+  );
+}
+
+export default PageContainer;
